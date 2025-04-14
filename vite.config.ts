414bbe947a24be51f53a0ecd6e251586944f193b
Jamal Hasuev <metafra@mail.ru>
2025-04-15 02:34:18 +0300

    fix build options

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  base: './', // Это важно для корректной работы с хэш-роутингом
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -20,6 +21,18 @@ export default defineConfig({
       scss: {
         // Удаляем глобальный импорт, который вызывает циклическую зависимость
         // additionalData: `@use "@/app/styles/variables.scss" as *;`
+      }
+    }
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
       }
     }
   },
